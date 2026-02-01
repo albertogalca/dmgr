a9312e87c911fe01435b1a24723022dd62434e36
Alberto Gallego <albertogc2793@gmail.com>
2026-02-01 15:32:08 +0100

    release homebrew implemented

--- Formula/dmgr.rb
+++ Formula/dmgr.rb
@@ -1,16 +1,16 @@
 class Dmgr < Formula
   desc "macOS app distribution manager - archive, sign, notarize, and distribute"
   homepage "https://github.com/albertogalca/dmgr"
-  version "0.1.0"
+  version "1.0.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/albertogalca/dmgr/releases/download/v#{version}/dmgr-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_ARM64"
+      url "https://github.com/albertogalca/dmgr/releases/download/v#{version}/dmgr-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "REPLACE_ARM64_SHA256"
     else
-      url "https://github.com/albertogalca/dmgr/releases/download/v#{version}/dmgr-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_X86_64"
+      url "https://github.com/albertogalca/dmgr/releases/download/v#{version}/dmgr-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "REPLACE_X86_64_SHA256"
     end
   end
 
@@ -19,6 +19,6 @@ class Dmgr < Formula
   end
 
   test do
-    system "#{bin}/dmgr", "--version"
+    system "#{bin}/dmgr", "--help"
   end
 end
